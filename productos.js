7128c6cf68e35d037fdd90fb7f8619e0a9d01bb7
SmileMarket <bonellimaximiliano@gmail.com>
2025-05-25 17:52:21 -0300

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,138 @@
+// productos.js
+const productos = [
+  {
+    nombre: "Adaptador fresas",
+    precio: 6000,
+    imagen: "img/Adaptador_fresa.jpg"
+  },
+  {
+    nombre: "Endo Z - EN PROMO!",
+    precio: 5500,
+    imagen: "img/EndoZ.jpg"
+  },
+  {
+    nombre: "Espaciador",
+    precio: 5000,
+    imagen: null
+  },
+  {
+    nombre: "Espejo de Rhodio",
+    precio: 3400,
+    imagen: null
+  },
+  {
+    nombre: "Fresa cónica - Corta",
+    precio: 4400,
+    imagen: null
+  },
+  {
+    nombre: "Fresa cónica - Larga",
+    precio: 4800,
+    imagen: null
+  },
+  {
+    nombre: "Fresa de bola",
+    precio: 4400,
+    imagen: null
+  },
+  {
+    nombre: "Fresa de cavidad (acero)",
+    precio: 3000,
+    imagen: null
+  },
+  {
+    nombre: "Fresa de fisura",
+    precio: 3000,
+    imagen: null
+  },
+  {
+    nombre: "Fresa de carburo de tungsteno (1 UND)",
+    precio: 3000,
+    imagen: null
+  },
+  {
+    nombre: "Fresa de diamante (1 UND)",
+    precio: 3000,
+    imagen: null
+  },
+  {
+    nombre: "Fresa llama",
+    precio: 4400,
+    imagen: null
+  },
+  {
+    nombre: "Fresa óvalo",
+    precio: 4400,
+    imagen: null
+  },
+  {
+    nombre: "Fresa troncocónica - Corta",
+    precio: 4400,
+    imagen: null
+  },
+  {
+    nombre: "Fresa troncocónica - Larga",
+    precio: 4800,
+    imagen: null
+  },
+  {
+    nombre: "Fresa troncocónica invertida",
+    precio: 4400,
+    imagen: null
+  },
+  {
+    nombre: "Gasa estéril",
+    precio: 2500,
+    imagen: null
+  },
+  {
+    nombre: "Jeringa Carpule",
+    precio: 5000,
+    imagen: null
+  },
+  {
+    nombre: "Jeringa triple",
+    precio: 6000,
+    imagen: null
+  },
+  {
+    nombre: "Jeringas para impresión",
+    precio: 2500,
+    imagen: null
+  },
+  {
+    nombre: "Mandril para discos",
+    precio: 1500,
+    imagen: null
+  },
+  {
+    nombre: "Micromotor",
+    precio: 17000,
+    imagen: null
+  },
+  {
+    nombre: "Pack fresas x5 unidades (a elección)",
+    precio: 14500,
+    imagen: null
+  },
+  {
+    nombre: "Posicionador de película",
+    precio: 6500,
+    imagen: null
+  },
+  {
+    nombre: "Sonda de exploración",
+    precio: 3500,
+    imagen: null
+  },
+  {
+    nombre: "Tijera de Metzembaum",
+    precio: 5500,
+    imagen: null
+  },
+  {
+    nombre: "Turbina",
+    precio: 17000,
+    imagen: null
+  }
+];
