27b21d29da8063f71f0ad36a0c553b85b29260ed
SmileMarket <bonellimaximiliano@gmail.com>
2025-05-29 10:39:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -173,17 +173,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // === Buscador ===
   const inputBuscador = document.getElementById('buscador');
   inputBuscador.addEventListener('input', () => {
-const termino = inputBuscador.value.trim().toLowerCase();
-if (termino === '') {
-  document.querySelectorAll('.producto').forEach(producto => {
-    producto.style.display = '';
-    const nombreElem = producto.querySelector('h3');
-    const categoriaElem = producto.querySelector('.categoria-texto');
-    nombreElem.innerHTML = producto.dataset.nombre;
-    categoriaElem.innerHTML = producto.dataset.categoria;
-  });
-  return;
-}
+    const termino = inputBuscador.value.toLowerCase();
     const productosDOM = document.querySelectorAll('.producto');
 
     productosDOM.forEach(producto => {
