66c287b45e1c9883ddfb216a0d2e7c825c75023a
SmileMarket <bonellimaximiliano@gmail.com>
2026-03-17 19:32:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -447,6 +447,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 
     calcularResumen();
+document.getElementById('checkout-total').textContent = '$' + totalGlobal.toLocaleString();
     mostrarProductosRelacionados();
   });
 
